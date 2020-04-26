3e6a37564d5aa52a7687a867a7faaabdb08a493d
EHuq <53830580+EHuq@users.noreply.github.com>
2020-04-26 18:44:17 -0400

    Update Contacts.js

--- src/pages/Contacts.js
+++ src/pages/Contacts.js
@@ -16,94 +16,104 @@ class Contacts extends React.Component {
     };
   }
 
-  // handleChange = event => {
-  //   const target = event.target;
-  //   const value = target.type === "checkbox" ? target.checked : target.value;
-  //   const name = target.name;
+  handleChange = (event) => {
+    const target = event.target;
+    const value = target.type === "checkbox" ? target.checked : target.value;
+    const name = target.name;
 
-  //   this.setState({
-  //     [name]: value
-  //   });
-  // };
+    this.setState({
+      [name]: value,
+    });
+  };
 
-  // handleSubmit = event => {
-  //   event.preventDefault();
+  handleSubmit = (event) => {
+    event.preventDefault();
 
-  //   this.setState({
-  //     disable: true
-  //   });
+    this.setState({
+      disable: true,
+    });
 
-  //   Axios.post("http://localhost:3030/api/email", this.state)
-  //     .then(res => {
-  //       if (res.data.success) {
-  //         this.setState({
-  //           disabled: false,
-  //           emailSent: true
-  //         });
-  //       } else {
-  //         this.setState({
-  //           disabled: false,
-  //           emailSent: false
-  //         });
-  //       }
-  //     })
+    Axios.post("http://localhost:3030/api/email", this.state)
+      .then((res) => {
+        if (res.data.success) {
+          this.setState({
+            disabled: false,
+            emailSent: true,
+          });
+        } else {
+          this.setState({
+            disabled: false,
+            emailSent: false,
+          });
+        }
+      })
 
-  //     .catch(err => {
-  //       this.setState({
-  //         disabled: false,
-  //         emailSent: false
-  //       });
-  //     });
-  // };
+      .catch((err) => {
+        this.setState({
+          disabled: false,
+          emailSent: false,
+        });
+      });
+  };
 
   render() {
     return (
       <div className="contacts">
         <Summary title={this.props.title} />
 
-        <html lang="en">
-          <head>
-            <meta charset="UTF-8" />
-            <title>Contact</title>
-            <script src="https://unpkg.com/react@16/umd/react.development.js"></script>
-            <script src="https://unpkg.com/react-dom@16/umd/react-dom.development.js"></script>
-            <script src="https://unpkg.com/babel-standalone@6.15.0/babel.min.js"></script>
-          </head>
-          <body>
-            <form name="contact" netlify netlify-honeypot="bot-field" hidden>
-              <input type="text" name="name" />
-              <input type="email" name="email" />
-              <textarea name="message"></textarea>
-            </form>
+        <Content>
+          <Form /* onSubmit={this.handleSubmit} */ netlify>
+            <Form.Group>
+              <Form.Label htmlFor="full-name">Full name</Form.Label>
+              <Form.Control
+                id="full-name"
+                name="name"
+                type="text"
+                // value={this.state.name}
+                // onChange={this.handleChange}
+              />
+            </Form.Group>
 
-            <div id="root"></div>
-            <script type="text/babel">
-              ReactDOM.render(
-              <form name="contact" method="post">
-                <input type="hidden" name="form-name" value="contact" />
-                <p>
-                  <label>
-                    Your Name: <input type="text" name="name" />
-                  </label>
-                </p>
-                <p>
-                  <label>
-                    Your Email: <input type="email" name="email" />
-                  </label>
-                </p>
-                <p>
-                  <label>
-                    Message: <textarea name="message"></textarea>
-                  </label>
-                </p>
-                <p>
-                  <button type="submit">Send</button>
-                </p>
-              </form>
-              , document.getElementById("root") );
-            </script>
-          </body>
-        </html>
+            <Form.Group>
+              <Form.Label htmlFor="email">Email</Form.Label>
+              <Form.Control
+                id="email"
+                name="email"
+                type="email"
+                // value={this.state.email}
+                // onChange={this.handleChange}
+              />
+            </Form.Group>
+
+            <Form.Group>
+              <Form.Label htmlFor="message">Message</Form.Label>
+              <Form.Control
+                id="message"
+                name="message"
+                as="textarea"
+                rows="3"
+                // value={this.state.message}
+                // onChange={this.handleChange}
+              />
+            </Form.Group>
+
+            <Button
+              className="d-inline-block"
+              variant="primary"
+              type="submit"
+              disable={this.state.disabled}
+            >
+              Send
+            </Button>
+
+            {this.state.emailSent == true && (
+              <p className="d-inline success-msg">Email Sent</p>
+            )}
+            {this.state.emailSent == false && (
+              <p className="d-inline err-msg">Email Did Not Send</p>
+            )}
+          </Form>
+        </Content>
       </div>
     );
   }
