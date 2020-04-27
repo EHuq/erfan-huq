a32883b1952556f832d7bd6ecf7c60a951e7e68c
EHuq <53830580+EHuq@users.noreply.github.com>
2020-04-27 15:45:44 -0400

    Update Contacts.js

--- src/pages/Contacts.js
+++ src/pages/Contacts.js
@@ -61,27 +61,29 @@ class Contacts extends React.Component {
       <div className="contacts">
         <Summary title={this.props.title} />
         <input type="hidden" name="form-name" value="contact" />
-        <form name="contact" method="post">
-          <input type="hidden" name="form-name" value="contact" />
-          <p>
-            <label>
-              Your Name: <input type="text" name="name" />
-            </label>
-          </p>
-          <p>
-            <label>
-              Your Email: <input type="email" name="email" />
-            </label>
-          </p>
-          <p>
-            <label>
-              Message: <textarea name="message"></textarea>
-            </label>
-          </p>
-          <p>
-            <button type="submit">Send</button>
-          </p>
-        </form>
+        <Content className="contacts">
+          <form name="contact" method="post">
+            <input type="hidden" name="form-name" value="contact" />
+            <p>
+              <label>
+                Name <input type="text" name="name" />
+              </label>
+            </p>
+            <p>
+              <label>
+                Email <input type="email" name="email" />
+              </label>
+            </p>
+            <p>
+              <label>
+                Message <textarea name="message"></textarea>
+              </label>
+            </p>
+            <p>
+              <button type="submit">Send</button>
+            </p>
+          </form>
+        </Content>
       </div>
     );
   }
